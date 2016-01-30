199762d2321f34ff95f855e7fdf6699b4fa2e9a2
Max Maurin <mxm4158@rit.edu>
2016-01-30 13:31:35 -0500

    add new view form

--- SandboxFuckery/SidebarMenu/SidebarMenu/AddNewViewController.swift
+++ SandboxFuckery/SidebarMenu/SidebarMenu/AddNewViewController.swift
@@ -12,14 +12,13 @@ class AddNewViewController: UIViewController  {
     
     @IBOutlet weak var menuButton:UIBarButtonItem!
     
+    @IBOutlet weak var casualBool: UILabel!
+    @IBOutlet weak var PartyBool: UILabel!
+    @IBOutlet weak var interviewBool: UILabel!
+    @IBOutlet weak var addANewLabel: UILabel!
+    @IBOutlet weak var segmentedTypePicker: UISegmentedControl!
     
-    @IBAction func cancel(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
     
-    @IBAction func done(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
     
     
     override func viewDidLoad() {
@@ -40,6 +39,29 @@ class AddNewViewController: UIViewController  {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func selectType(sender: AnyObject) {
+        switch segmentedTypePicker.selectedSegmentIndex
+        {
+        case 0:
+            addANewLabel.text = "First selected";
+        case 1:
+            addANewLabel.text = "Second Segment selected";
+        case 2:
+            addANewLabel.text = "Third Segment is on";
+        case 3:
+            addANewLabel.text = "Fourth Seg Yo";
+        default:
+            break; 
+        }
+    }
+    
+    
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func done(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
