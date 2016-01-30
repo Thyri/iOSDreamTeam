eb5b65f819b517996057063b96a45784b9a7212e
Max Maurin <mxm4158@rit.edu>
2016-01-29 20:51:48 -0500

    let there be hamper

---
+++ SandboxFuckery/SidebarMenu/SidebarMenu/AddNewViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddNewViewController.swift
+//  SidebarMenu
+//
+//  Created by Max Maurin on 1/29/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class AddNewViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate  {
+    
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+    
+    let pickerData = ["Mozzarella","Gorgonzola","Provolone","Brie","Maytag Blue","Sharp Cheddar","MonterreyJack","Stilton","Gouda","Goat Cheese", "Asiago"]
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+}
