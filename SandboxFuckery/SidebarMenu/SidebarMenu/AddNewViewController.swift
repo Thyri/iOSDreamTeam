c211deb1047c9a21f09691a8dbddf64a71205bf3
Max Maurin <mxm4158@rit.edu>
2016-01-30 17:09:40 -0500

    they see me scrollin', they hatin'

--- SandboxFuckery/SidebarMenu/SidebarMenu/AddNewViewController.swift
+++ SandboxFuckery/SidebarMenu/SidebarMenu/AddNewViewController.swift
@@ -8,16 +8,11 @@
 
 import UIKit
 
-class AddNewViewController: UIViewController  {
+class AddNewViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
     
     @IBOutlet weak var menuButton:UIBarButtonItem!
-    
-    @IBOutlet weak var casualBool: UILabel!
-    @IBOutlet weak var PartyBool: UILabel!
-    @IBOutlet weak var interviewBool: UILabel!
-    @IBOutlet weak var addANewLabel: UILabel!
-    @IBOutlet weak var segmentedTypePicker: UISegmentedControl!
-    
+    @IBOutlet weak var colorCollection: UICollectionView!
+    let colorChoices = [0xE74C3C, 0xC0392B, 0xD35400, 0xE67E22, 0xF1C40F, 0x2ECC71, 0x27AE60, 0x1ABC9C, 0x16A085, 0x3498D8, 0x2980B9, 0x32295E, 0x2C3E50, 0x9B59B6, 0x8E44AD, 0x252525, 0xBDC3C7, 0xEBEBEB]
     
     
     
@@ -32,28 +27,45 @@ class AddNewViewController: UIViewController  {
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
         // Do any additional setup after loading the view.
+        colorCollection.backgroundColor = UIColor.whiteColor()
+        colorCollection.dataSource = self
+        colorCollection.delegate = self
+        colorCollection.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+                
+    }
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
+        let currentColor = UInt(colorChoices[indexPath.row])
+        cell.backgroundColor = UIColorFromRGB(currentColor)
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let cell = collectionView.cellForItemAtIndexPath(indexPath)
+        let currentColor = UInt(colorChoices[indexPath.row])
+        colorCollection.backgroundColor = UIColorFromRGB(currentColor)
+        //cell?.backgroundColor = UIColorFromRGB(0xE74c3c)
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colorChoices.count
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func selectType(sender: AnyObject) {
-        switch segmentedTypePicker.selectedSegmentIndex
-        {
-        case 0:
-            addANewLabel.text = "First selected";
-        case 1:
-            addANewLabel.text = "Second Segment selected";
-        case 2:
-            addANewLabel.text = "Third Segment is on";
-        case 3:
-            addANewLabel.text = "Fourth Seg Yo";
-        default:
-            break; 
-        }
-    }
+
     
     
     @IBAction func cancel(sender: AnyObject) {
