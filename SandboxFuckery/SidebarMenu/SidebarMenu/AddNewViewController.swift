73f32e64b231b1682b1cba0f5c320e53f4c9b870
Max Maurin <mxm4158@rit.edu>
2016-01-30 19:35:04 -0500

    second collection added, still incapable of viewing icons

--- SandboxFuckery/SidebarMenu/SidebarMenu/AddNewViewController.swift
+++ SandboxFuckery/SidebarMenu/SidebarMenu/AddNewViewController.swift
@@ -12,14 +12,18 @@ class AddNewViewController: UIViewController, UICollectionViewDelegate, UICollec
     
     @IBOutlet weak var menuButton:UIBarButtonItem!
     @IBOutlet weak var colorCollection: UICollectionView!
-    let colorChoices = [0xE74C3C, 0xC0392B, 0xD35400, 0xE67E22, 0xF1C40F, 0x2ECC71, 0x27AE60, 0x1ABC9C, 0x16A085, 0x3498D8, 0x2980B9, 0x32295E, 0x2C3E50, 0x9B59B6, 0x8E44AD, 0x252525, 0xBDC3C7, 0xEBEBEB]
+    @IBOutlet weak var articleCollection: UICollectionView!
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var mainImage: UIImageView!
     
+    let colorChoices = [0xE74C3C, 0xC0392B, 0xD35400, 0xE67E22, 0xF1C40F, 0x2ECC71, 0x27AE60, 0x1ABC9C, 0x16A085, 0x3498D8, 0x2980B9, 0x32295E, 0x2C3E50, 0x9B59B6, 0x8E44AD, 0x252525, 0xBDC3C7, 0xEBEBEB]
+    let articleChoices: [UIImage] = [UIImage(named: "tag")!, UIImage(named: "Shirt-100")!, UIImage(named: "Pants-100")!, UIImage(named: "Shoes-100")!, UIImage(named: "Shirt-100")!, UIImage(named: "Pants-100")!, UIImage(named: "Shoes-100")!, UIImage(named: "Shirt-100")!, UIImage(named: "Pants-100")!, UIImage(named: "Shoes-100")!, UIImage(named: "Shirt-100")!, UIImage(named: "Pants-100")!, UIImage(named: "Shoes-100")!, UIImage(named: "Shirt-100")!, UIImage(named: "Pants-100")!]
     
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-
+        
         
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
@@ -30,8 +34,14 @@ class AddNewViewController: UIViewController, UICollectionViewDelegate, UICollec
         colorCollection.backgroundColor = UIColor.whiteColor()
         colorCollection.dataSource = self
         colorCollection.delegate = self
-        colorCollection.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-                
+        colorCollection.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "ColorCell")
+        
+        articleCollection.backgroundColor = UIColor.whiteColor()
+        articleCollection.dataSource = self
+        articleCollection.delegate = self
+        articleCollection.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "ArticleCell")
+        
+        
     }
     
     func UIColorFromRGB(rgbValue: UInt) -> UIColor {
@@ -44,28 +54,64 @@ class AddNewViewController: UIViewController, UICollectionViewDelegate, UICollec
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
-        let currentColor = UInt(colorChoices[indexPath.row])
-        cell.backgroundColor = UIColorFromRGB(currentColor)
+        var cell = UICollectionViewCell()
+        
+        if collectionView == colorCollection{
+            cell = collectionView.dequeueReusableCellWithReuseIdentifier("ColorCell", forIndexPath: indexPath)
+            let currentColor = UInt(colorChoices[indexPath.row])
+            cell.backgroundColor = UIColorFromRGB(currentColor)
+            
+        } else if collectionView == articleCollection {
+            cell = collectionView.dequeueReusableCellWithReuseIdentifier("ArticleCell", forIndexPath: indexPath)
+            //imageTile.image = articleChoices[indexPath.row]
+            let imageTile = UIImageView()
+            cell.addSubview(imageTile)
+            imageTile.image = articleChoices[indexPath.row]
+            cell.backgroundColor = UIColor.blueColor()
+            
+            
+        } else {
+            assertionFailure("collectionview cellforindex not exhaustive")
+            
+        }
+        
         return cell
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        let cell = collectionView.cellForItemAtIndexPath(indexPath)
-        let currentColor = UInt(colorChoices[indexPath.row])
-        colorCollection.backgroundColor = UIColorFromRGB(currentColor)
-        //cell?.backgroundColor = UIColorFromRGB(0xE74c3c)
+        var cell = UICollectionViewCell()
+        if collectionView == colorCollection{
+            let cell = collectionView.cellForItemAtIndexPath(indexPath)
+            let currentColor = UInt(colorChoices[indexPath.row])
+            mainImage.backgroundColor = UIColorFromRGB(currentColor)
+        } else if collectionView == articleCollection {
+            let cell = collectionView.cellForItemAtIndexPath(indexPath)
+            mainImage.image = articleChoices[indexPath.row]
+        } else {
+            assertionFailure("collectionview cellforindex not exhaustive")
+        }
+        
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return colorChoices.count
+        var countValue = 0
+        if collectionView == colorCollection{
+            countValue = colorChoices.count
+            
+        } else if collectionView == articleCollection {
+            countValue = articleChoices.count
+        } else {
+            assertionFailure("collectionview cellforindex not exhaustive")
+            
+        }
+        return countValue
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     
     @IBAction func cancel(sender: AnyObject) {
@@ -75,5 +121,5 @@ class AddNewViewController: UIViewController, UICollectionViewDelegate, UICollec
     @IBAction func done(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
-
+    
 }
